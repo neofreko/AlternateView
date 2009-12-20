9df707a2a654a819cbfe0588b5089c5cf914d162
Your Name <akhmadf@gmail.com>
2009-12-20 22:06:49 +0700

    proof of concept using gloda to create alternative view fro message list

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.alternateview.boolpref", false);
+pref("extensions.alternateview.intpref", 0);
+pref("extensions.alternateview.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.akhmadf@gmail.com.description", "chrome://alternateview/locale/overlay.properties");
